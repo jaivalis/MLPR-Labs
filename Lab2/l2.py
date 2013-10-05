d017e1319c54452771469121ebf8203d59d3e0df
jaivalis <johnaivalis@gmail.com>
2013-10-06 01:11:05 +0200

    W is correct moved to script file

--- Lab2/l2.py
+++ Lab2/l2.py
@@ -1,6 +1,7 @@
 import gzip, cPickle
 import numpy as np
 import pylab as plt
+from math import exp, log1p
 
 def load_mnist():
 	f = gzip.open('mnist.pkl.gz', 'rb')
@@ -8,56 +9,110 @@ def load_mnist():
 	f.close()
 	return data
 
+def plot_digits(data, numcols, shape=(28,28)):
+    numdigits = data.shape[0]
+    numrows = int(numdigits/numcols)
+    for i in range(numdigits):
+        plt.subplot(numrows, numcols, i)
+        plt.axis('off')
+        plt.imshow(data[i].reshape(shape), interpolation='nearest', cmap='Greys')
+    plt.show()
+
+def plot_digit(digit) :
+    plt.imshow(digit.reshape(28,28), interpolation='nearest', cmap='Greys')
+    plt.show()
+
 (x_train, t_train), (x_valid, t_valid), (x_test, t_test) = load_mnist()
 
-def plot_digits(data, numcols, shape=(28,28)):
-	numdigits = data.shape[0]
-	numrows = int(numdigits/numcols)
-	for i in range(numdigits):
-		plt.subplot(numrows, numcols, i)
-		plt.axis('off')
-		plt.imshow(data[i].reshape(shape), interpolation='nearest', cmap='Greys')
-	plt.show()
+
+
+
+# x is a vector: 784         (datapoint)
+# t is a scalar              (corresponding class)
+# W is a matrix: 784 x 10    (weights)
+# b is a vector: 10          (bias)
+#
+# returns a matrix: 784 x 10 (partial derivative of log likelihood wrt w)
+# returns a vector: 10       (partial derivative of log likelihood wrt b)
+def logreg_gradient(x, t, W, b): # log_Q -> Z -> log_P -> delta
+    log_Q = []
+    log_P = []
+    partial_derivative_logLikelihood_b = []
+    partial_derivative_logLikelihood_W = []
+
+    Z = 0.0 # Z = normalizing factor
+    for j in range(0,10):
+        log_q = np.dot(np.transpose(W)[j], x) + b[j]
+        log_Q.append(log_q)
+        Z += exp(log_Q[j])
+
+        log_P.append(log_Q[j] - log1p(Z))
+                
+        if j == t:
+            partial_derivative_logLikelihood_b.append(1 - exp(log_Q[j])/Z)
+        else:
+            partial_derivative_logLikelihood_b.append(exp(log_Q[j])/Z)
+        
     
-plot_digits(x_train[0:8], numcols=4)
+    partial_derivative_logLikelihood_W = np.outer(x, partial_derivative_logLikelihood_b)
+    
+    return partial_derivative_logLikelihood_W, partial_derivative_logLikelihood_b
+    
+# x_train is a matrix: 50000 x 784 (dataset)
+# t_train is a vector: 50000       (class of x)
+# W       is a matrix: 784 x 10    (weights)
+# b       is a vector: 10          (bias for each class)
+def sgd_iter(x_train, t_train, W, b):
+    alpha = 1E-4      # learning rate
+    
+    x_trainIndex = np.arange(len(x_train), dtype = int)
+    np.random.shuffle(x_trainIndex) # shuffle indices
 
-# j : class
-# x : image for some datapoint
-# b : 
-# W : 
-# Returns conditional probability of class label t given image x 
-# for some datapoint.
-def get_conditional_probability_of_class(j, x, b, W):
-	b_j = 0 # TODO
-	w_j = W[j]
-	q_j = get_unnormalized_probability_of_class(j, x, w_j, b_j)
-	Z = get_normalizing_factor(q)
-	return q_j / Z
+    for xIndex in x_trainIndex:
+        x = x_train[xIndex]
+        t = t_train[xIndex]
+        
+        W = W + alpha * logreg_gradient(x, t, W, b)[0]
+        
+    return W
 
-# Returns Z
-def get_normalizing_factor(q):
-	Z = 0
-	for i in q:
-		Z = Z + q
-	return Z
+# x is a vector: 784         (datapoint)
+# t is a scalar: 1           (class of x)
+# W is a matrix: 784 x 10    (weights)
+# b is a vector: 10          (bias for each class)
+# returns conditional log probability(scalar)
+def get_log_P(x, t, W, b):
+    log_Q = []
+    for i in range(0,10):
+        log_q = np.dot(np.transpose(W)[i], x) + b[i]
+        log_Q.append(log_q)
+    Z = 0.0
+    for i in range (0, 10): # Z = normalizing factor        
+        Z += exp(log_Q[i])
+    log_p = log_Q[t] - log1p(Z)
+    return log_p
 
-# j   : class
-# x   : image for some datapoint
-# w_j : j-th column of matrix W
-# b_j : j-th element of vector w
-# Returns [q_j] the unnormalized probability of the class j (q_j)
-def get_unnormalized_probability_of_class(j, x, w_j, b_j):
-	q_j = exp( np.transpose(w_j).dot(x) + b_j )
-	return q_j
+def plot_training(handful, x_train, t_train, x_valid, t_valid):
+    W = np.zeros((784,10))
+    b = np.zeros(10)
+    
+    point = 0
+    for i in range(0, handful):
+        W = sgd_iter(x_train, t_train, W, b)
+    
+        for xIndex in range(0, len(x_valid)):
+            x = x_valid[xIndex]
+            t = t_valid[xIndex]
+            log_P = get_log_P(x, t, W, b)
+            if i%1000 == 0:
+                plt.plot(point, log_P, 'o')
+            point += 1
+    plt.show()
+    return W
+    
+def visualize_weights(W):
+    plot_digits(W, 5, shape=(28,28))
 
-# Task 1.1.2
-# x : datapoint
-# t : class
-# Returns [q_j] the gradient wrt w & b of the log-likelihood for x,t
-def logreg_gradient(x, t, w, b):
-	return 0 # TODO
-
-# Task 1.1.3
-def sgt_iter(x_train, t_train, w, b):
-	learning_rate = 1E-4
-	return None # TODO
+(x_train, t_train), (x_valid, t_valid), (x_test, t_test) = load_mnist()
+W = plot_training(1, x_train, t_train, x_valid, t_valid)
+visualize_weights(np.transpose(W))
